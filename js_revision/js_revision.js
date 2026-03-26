64c1932f11a1a839723d2af84c47fb21fc541025
Chandan Yadav <chandanyadavofficial06@gmail.com>
2026-03-26 19:01:41 +0530

    variable revision

---
+++ js_revision/js_revision.js
@@ -0,0 +1,35 @@
+/***********************************Variale practice******************** */
+
+let name = "Chandan";
+let num = 6;
+console.log(name + num);
+
+console.log(typeof (name+num));
+
+const a = {
+ name : "Chandan",
+ class : "MSc.IT.",
+ Roll_no: 217
+}
+
+a['College'] = "RADAv";
+a['name'] = "Sandesh";
+
+console.log(a);
+
+
+const dictionary = {
+  Happy: "Feeling good and joyful",
+  Sad: "Feeling unhappy or down",
+  Brave: "Not afraid to face danger",
+  Kind: "Being nice and caring",
+  Fast: "Moving quickly",
+  Slow: "Moving without speed",
+  Easy: "Not difficult",
+  Hard: "Difficult to do",
+  Big: "Large in size",
+  Small: "Little in size"
+};
+console.log(dictionary["Brave"]);
+console.log(dictionary.Hard);
+
