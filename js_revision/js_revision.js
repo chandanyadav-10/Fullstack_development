06f5951e6d26f39ef542522522814b496399dda3
Chandan Yadav <chandanyadavofficial06@gmail.com>
2026-03-28 21:24:20 +0530

    js revision

--- js_revision/js_revision.js
+++ js_revision/js_revision.js
@@ -116,10 +116,236 @@
 
 // console.log((age>18) ? "You can drive car" : "You cannot drive a car");
 
-let num = Number(prompt("Enter a number:"));
+// let num = Number(prompt("Enter a number:"));
 
-let result = (num % 2 === 0 && num % 3 === 0)
-  ? "Divisible by both 2 or 3"
-  : "Not divisible by both 2 or 3";
+// let result = (num % 2 === 0 && num % 3 === 0)
+//   ? "Divisible by both 2 or 3"
+//   : "Not divisible by both 2 or 3";
 
-console.log(result);
+// console.log(result);
+
+
+/*****************************Loops and FUnction**************************************** */
+
+// simple for looop----------------------------------------
+
+// for (let i = 1; i <= 5; i++) {
+//     console.log("Number:", i);
+// }
+
+// simple program to demonstrate for in loop--------------------------------
+
+// let student = {
+//     name: "Chandan",
+//     age: 21,
+//     course: "MCA"
+// };
+
+// for (let key in student) {
+//     console.log(key + ": " + student[key]);
+// }
+
+// let numbers = [10, 20, 30, 40];
+
+// for (let index in numbers) {
+//     console.log("Index:", index, "Value:", numbers[index]);
+// }
+
+
+// simple program to demonstrate for of loop--------------------------------
+
+// let fruits = ["Apple", "Banana", "Mango"]; // for of loop doesn't work directly with object
+
+// for (let fruit of fruits) {
+//     console.log(fruit);
+// }
+
+// this will give you error====================================
+// let student = {
+//     name: "Chandan",
+//     age: 21
+// };
+
+// for (let value of student) {
+//     console.log(value);
+// } 
+
+// correct way using object.values>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// let student = {
+//     name: "Chandan",
+//     age: 21,
+//     course: "MCA"
+// };
+
+// for (let value of Object.values(student)) {
+//     console.log(value);
+// }
+
+// for (let [key, value] of Object.entries(student)) {
+//     console.log(key + ": " + value);
+// }
+
+// Object + for...in → direct ✅
+// Object + for...of → use Object.values() or Object.entries() ✅
+
+
+//simple while loop program---------------------------------------------------
+
+// let i = 1;
+
+// while (i <= 5) {
+//     console.log("Number:", i);
+//     i++;
+// }
+
+//simple program to demo do while loop-------------------------------------------------
+
+// let i = 1;
+
+// do {
+//     console.log("Number:", i);
+//     i++;
+// } while (i <= 5);
+
+//simple function program--------------------------------------------
+
+// function greet() {
+//     console.log("Hello, welcome!");
+// }
+
+// greet(); // calling the function
+
+// function greet(name) {
+//     console.log("Hello " + name);
+// }
+
+// greet("Chandan");
+
+
+/*******************************Practice loop and fucntion*************************** */
+
+//print marks of students in obj using for loop-------------------------------------
+
+// let marks = {
+//     Harry: 98,
+//     Rohan: 70,
+//     Akash: 75, 
+//     Chandan: 85,
+//     Sandesh: 80
+// }
+
+// for(let student in marks){
+//     console.log(student + ": " + marks[student])
+// }
+
+// for(let i = 0; i<Object.keys(marks).length; i++){
+//     console.log("Marks of " + Object.keys(marks)[i] + " are " + marks[Object.keys(marks)[i]]);
+// }
+
+
+// let cn = 52;
+// let i;
+
+// while(i != cn){
+//     console.log("Try Again!!");
+//     i= prompt("Enter a number");
+// }
+
+// console.log("You have Entered a correct number");
+
+
+// let mean = (a,b,c,d) =>{
+//     return (a+b+c+d)/4;
+// }
+// console.log(mean(10,20,30,40));
+
+
+/**********************************Stirng and Array Methods*********************************** */
+
+
+//EScaping characters in string-------------------------
+// let name = "Chandan\nYadav";
+// console.log(name);
+// console.log(name.length);
+
+// let str = "Chandan\'s laptop";
+// console.log(str);
+
+// let text = "Hello\nChandan\tWelcome to \"JavaScript\"";
+// console.log(text); 
+
+// let name1 = "Chandan";
+// let name2 = "Yadav";
+// let name3  = name1 + " " + name2;
+// console.log(name3);
+// let name4 = `${name1} ${name2}`;
+// console.log(name4);
+// let name5 = name1.concat(" ", name2, " is a good boy");
+// console.log(name5);
+
+// let name = "Chandan Yadav";
+// console.log(name.length);
+// console.log(name.toUpperCase());
+
+// console.log(name[0]);
+// console.log(name[1]);
+// console.log(name[2]);
+// console.log(name[3]);
+// console.log(name[4]);
+// console.log(name[5]);
+// console.log(name[6]);
+// console.log(name[7]);
+// console.log(name[8]);
+// console.log(name[9]);
+// console.log(name[10]);
+// console.log(name[11]);
+// console.log(name[12]);
+
+// console.log(name.charAt(0));
+// console.log(name.charAt(1));
+// console.log(name.charAt(2));
+// console.log(name.charAt(3));
+// console.log(name.charAt(4));
+// console.log(name.charAt(5));
+// console.log(name.charAt(6));
+// console.log(name.charAt(7));
+// console.log(name.charAt(8));
+// console.log(name.charAt(9));
+// console.log(name.charAt(10));
+// console.log(name.charAt(11));
+// console.log(name.charAt(12));
+
+let n = [1, 2, 3, 4, 5]; 
+console.log(n)
+console.log(n.length);
+console.log(n[0]);
+
+console.log(typeof n);
+
+let newArray = (n.toString());
+console.log(newArray);
+console.log(typeof newArray)  
+
+console.log(n.join("-"));
+// console.log(n.pop());
+// console.log(n);
+console.log(n.push(6));
+console.log(n);
+
+/***********************************************Practice Strings and Arrays************************************/
+
+// console.log("harr\"".length);
+
+// let amount = "Please enter the amount: 1000";
+// console.log(amount.length);
+// console.log(amount.slice(24));
+// let newAmount = (Number(amount.slice("Please enter the amount: ".length)));
+// console.log(newAmount);
+// console.log(typeof newAmount);
+// let name = "Chandan Yadav";
+// let newName = "B" + name.slice(1);
+// console.log(newName);
+
+// let newname2 = name.replace("Chandan", "Sandesh");
+// console.log(newname2);
