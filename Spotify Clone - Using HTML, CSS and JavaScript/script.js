d4e251b048e0110f8d52e79189c2bc60165f0d20
Chandan Yadav <chandanyadavofficial06@gmail.com>
2026-06-17 09:59:44 +0530

    Design playbar, seekbar and play music

--- Spotify Clone - Using HTML, CSS and JavaScript/script.js
+++ Spotify Clone - Using HTML, CSS and JavaScript/script.js
@@ -20,10 +20,28 @@ async function getSongs() {
   return songsArr;
 }
 
+// const playMusic = (track) => {
+//   let audio = new Audio("/songs/" + track)
+//   audio.play()
+// };
+
+let currentSong;
+
+const playMusic = (track) => {
+  if (currentSong) {
+    currentSong.pause();
+  }
+
+  currentSong = new Audio("/songs/" + track);
+
+  currentSong.play();
+};
+
 async function main() {
   //Get the list of all songs
   let songs = await getSongs();
 
+  //show all the songs in the playlist
   let songUL = document
     .querySelector(".songList")
     .getElementsByTagName("ul")[0];
@@ -35,11 +53,10 @@ async function main() {
       .replace(/\s+/g, " ")
       .replace(".mp3", "")
       .trim();
-
-    songUL.innerHTML += `<li>
+    songUL.innerHTML += `<li data-song="${song}">
               <img class="invert" src="music.svg" alt="">
               <div class="info">
-                <div>${cleanSong}</div>
+                <div>${cleanSong}"</div>
                 <div>Chandan</div>
               </div>
               <div class="playnow">
@@ -48,9 +65,25 @@ async function main() {
               </div>
   </li>`;
   }
-  //Play the first songs
-  let audio = new Audio(songs[0]);
-  // audio.play();
+  //Attach an event listener to each song
+  // Array.from(
+  //   document.querySelector(".songList").getElementsByTagName("li"),
+  // ).forEach((e) => {
+  //   console.log(e.querySelector(".info").firstElementChild.innerHTML);
+  //   playMusic(e.querySelector(".info").firstElementChild.innerHTML);
+  // });
+
+  Array.from(
+    document.querySelector(".songList").getElementsByTagName("li"),
+  ).forEach((e) => {
+    e.addEventListener("click", () => {
+      let track = e.dataset.song;
+
+      console.log(track);
+
+      playMusic(track);
+    });
+  });
 }
 
 main();
