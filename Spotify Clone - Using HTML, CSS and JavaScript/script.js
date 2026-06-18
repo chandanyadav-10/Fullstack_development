85e1cb7384cb85dd52686738e5516e06664074fb
Chandan Yadav <chandanyadavofficial06@gmail.com>
2026-06-18 18:44:54 +0530

    design and gave logic to seekbar

--- Spotify Clone - Using HTML, CSS and JavaScript/script.js
+++ Spotify Clone - Using HTML, CSS and JavaScript/script.js
@@ -53,6 +53,25 @@ const playMusic = (track) => {
   currentSong.addEventListener("timeupdate", () => {
     document.querySelector(".songtime").innerHTML =
       `${secondsToMinutesSeconds(currentSong.currentTime)} / ${secondsToMinutesSeconds(currentSong.duration)}`;
+    document.querySelector(".circle").style.left =
+      (currentSong.currentTime / currentSong.duration) * 100 + "%";
+  });
+
+  //Add an event listener to seekbar
+  document.querySelector(".seekbar").addEventListener("click", (e) => {
+    let percent = (e.offsetX / e.target.getBoundingClientRect().width) * 100;
+    console.log((document.querySelector(".circle").style.left = percent + "%"));
+    currentSong.currentTime = (currentSong.duration * percent) / 100;
+  });
+
+  //Add event listener to hamburger
+  document.querySelector(".hamburger").addEventListener("click", ()=>{
+    document.querySelector(".left").style.left = "0"
+  });
+
+  //Add event listener to close icon
+   document.querySelector(".closeIcon").addEventListener("click", ()=>{
+    document.querySelector(".left").style.left = "-100%"
   });
 
   currentSong.play();
@@ -104,6 +123,7 @@ async function main() {
               </div>
   </li>`;
   }
+
   //Attach an event listener to each song
   // Array.from(
   //   document.querySelector(".songList").getElementsByTagName("li"),
