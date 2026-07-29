96cebdd7265784aaeedae52efba56be0744f9dc5
Chandan Yadav <chandanyadavofficial06@gmail.com>
2026-07-29 20:28:00 +0530

    Array, Tuple and Enum in TS

--- TypeScript/src/arrayTupleEnumTS.ts
+++ TypeScript/src/arrayTupleEnumTS.ts
@@ -17,6 +17,61 @@ const cities: readonly string[] = ["Mumbai", "Banglore", "Delhi", "Hydrabad"];
 // cities.push("Pune")
 
 const table: number[][] = [
-    [1, 4, 7, 9],
-    [4, 6, 8, 2]
-]
+  [1, 4, 7, 9],
+  [4, 6, 8, 2],
+];
+
+let chaiTuple: [string, number];
+chaiTuple = ["Masala", 20];
+//chaiTuple = [20, "Masala"]; // We cannot use wrong format of defined tuples
+
+let userInfo: [string, number, boolean?];
+userInfo = ["Chandan", 104];
+userInfo = ["Chandan", 104, true];
+
+const location: readonly [number, number] = [23.67, 33.56];
+
+const chaiItems: [name: string, price: number] = ["Masala", 100];
+
+enum cupSize {
+  SMALL,
+  MEDIUM,
+  LARGE,
+}
+
+const size = cupSize.SMALL;
+
+enum Status {
+  PENDING = 100,
+  SERVED, //101
+  CANCELLED, //102
+}
+
+enum ChaiType {
+  MASALA = "masal",
+  GINGER = "ginger",
+}
+
+function makeChai(type: ChaiType) {
+  console.log(`Making: ${type}`);
+}
+
+makeChai(ChaiType.GINGER);
+// makeChai("masala")
+
+enum randomEnum {
+  ID = 1,
+  NAME = "Chai",
+}
+
+const enum sugars {
+  LOW = 1,
+  MEDIUM = 2,
+  HIGH = 3,
+}
+
+const s = sugars.MEDIUM
+
+//unexpected behaviour
+let t: [string, number] = ["Chandan", 104]
+t.push("SDE")
